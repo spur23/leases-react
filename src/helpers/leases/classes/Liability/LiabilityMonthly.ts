df2c9dd0a8caf4bd96d04270267569c88969c781
spur23 <spurius1987@gmail.com>
2021-03-11 08:51:32 -0500

    remove commented code

--- src/helpers/leases/classes/Liability/LiabilityMonthly.ts
+++ src/helpers/leases/classes/Liability/LiabilityMonthly.ts
@@ -22,43 +22,6 @@ export class LiabilityMonthly {
     this.payment = payment;
     this.principal = principal;
     this.endingBalance = endingBalance;
-    // this.date = date;
-    // this.beginningBalance = beginningBalance;
-    // this.interestRate = interestRate / 12;
-    // this.interestExpense = roundNumber(
-    //   this.beginningBalance * this.interestRate,
-    //   2
-    // );
-
-    // if (this.prepaid) {
-    //   // check if the month has a cash payment if it does not set the payment to 0
-    //   // else calculate the principal payment
-    //   if (payment === 0) {
-    //     this.principal = 0;
-    //     this.interestPayment = 0;
-    //   } else {
-    //     this.principal = payment - this.interestPayment;
-    //     this.interestPayment = interestPayment;
-    //   }
-
-    //   this.endingBalance = roundNumber(
-    //     this.beginningBalance +
-    //       this.interestExpense -
-    //       this.principal -
-    //       this.interestPayment,
-    //     2
-    //   );
-    // } else {
-    //   if (payment === 0) {
-    //     this.principal = 0;
-    //   } else {
-    //     this.principal = payment - this.interestExpense;
-    //   }
-    //   this.endingBalance = roundNumber(
-    //     this.beginningBalance - this.principal,
-    //     2
-    //   );
-    // }
   }
 
   getMonthlyData(): LiabilityMonthlyValues {
