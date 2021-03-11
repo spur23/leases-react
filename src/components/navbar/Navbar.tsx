83e24d1148c36044b47e7a78442e7184aeff4ee5
spur23 <spurius1987@gmail.com>
2021-03-11 17:39:16 -0500

    add styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,23 @@
 import { Link } from '@reach/router';
+import StyledNavbar from './StyledNavbar';
 
 const Navbar = () => {
   return (
-    <div className="navbar-container">
+    <StyledNavbar>
       <ul>
         <li>
           <Link to="/leases-react/">Home</Link>
+        </li>
+        <li>
           <Link to="/leases-react/create-lease">Create Lease</Link>
+        </li>
+        <li>
           <Link to="/leases-react/present-value-calculator">
             Present Value Calculator
           </Link>
         </li>
       </ul>
-    </div>
+    </StyledNavbar>
   );
 };
 
