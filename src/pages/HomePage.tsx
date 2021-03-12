c5ecb4d14591f44052b447e7c295290a05bad693
Jacob Sada <jacob.sada2@gmail.com>
2021-03-12 16:44:41 -0500

    add a welcome and some paragraphs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,16 @@
-import { RouteComponentProps } from '@reach/router';
+import { RouteComponentProps } from "@reach/router";
 
 const HomePage = (_props: RouteComponentProps) => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h4>Welcome!</h4>
+      <p>
+        This site is to help calculate a lease schedule in accordance with ASC
+        842.
+      </p>
+      <p>Please follow the links at the top</p>
+    </div>
+  );
 };
 
 export default HomePage;
